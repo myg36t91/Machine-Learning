175f7fd9dbc8ee495ab56fc3c5b7b29dd14fd663
XIAO, YU-YI <45507258+myg36t91@users.noreply.github.com>
2022-02-26 19:03:21 +0800

    Update regression.py

--- hw1/regression.py
+++ hw1/regression.py
@@ -1,18 +1,26 @@
-import torch, pandas, numpy
+import torch, pandas, numpy, cv2
 import matplotlib.pyplot as plt
 from torch import nn, optim
 
-data_file = pandas.read_csv('pokemon.csv_path')
+# show cat.jpg using cv2
+img = cv2.imread('D:\Downloads\hw1\data\cat.jpg')
+cv2.imshow('images', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows
 
+# show cat.jpg using plt
+img = plt.imread('D:\Downloads\hw1\data\cat.jpg')
+plt.imshow(img)
+plt.show()
+
+# linear regression
+data_file = pandas.read_csv('D:\Downloads\hw1\data\pokemon.csv')
 x = numpy.array(data_file['cp'])
 y = numpy.array(data_file['cp_new'])
-
 x = numpy.reshape(x, (x.size, 1))
 y = numpy.reshape(y, (y.size, 1))
-
 x = x.astype(numpy.float32)
 y = y.astype(numpy.float32)
-
 x = torch.tensor(x)
 y = torch.tensor(y)
 
@@ -31,21 +39,16 @@ loss_list = []
 
 epoch = 20
 for i in range(epoch):
+    opt.zero_grad()
     lossfunction = nn.MSELoss()
     loss = lossfunction(model(x), y)
     loss.backward()
-    opt.zero_grad()
     opt.step()
     loss_list.append(loss)
     print(loss.item())
 
 plt.figure(1)
-plt.plot(x, y, 'o', 'red')
-plt.show()
-
-plt.figure(1)
 plt.plot(loss_list, 'r')
-
 plt.figure(2)
 plt.plot(x, y, 'o', 'r')
 plt.plot(x, model(x).detach().numpy())
